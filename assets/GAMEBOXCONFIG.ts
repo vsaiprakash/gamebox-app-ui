cb03b5e6d599e07be58b5e712d922bb8f557ef65
V. Sai Prakash <vukkumsp@gmail.com>
2021-09-18 06:55:49 +0000

    Auto-generated commit [ci skip]

--- assets/GAMEBOXCONFIG.ts
+++ assets/GAMEBOXCONFIG.ts
@@ -1,7 +1,7 @@
 import { GameModel } from "src/app/core/models/game-model";
 import { CategoryModel } from "src/app/core/models/category-model";
 import { CarouselModel } from "src/app/core/models/carousel-model";
-
+import * as data from './../assets/games.json';
 
 export const CATEGORIES_LIST = [
   new CategoryModel("All","All Games","all"),
@@ -24,6 +24,7 @@ export const GAMEBOXCONFIG = {
     new GameModel( "assets/images/pokemon.jpeg", "Pokemon", "An RPG Pokemon Game", "pokemon", "https://jsemu2.github.io/gba/launcher.html#pokemonred", CATEGORIES_LIST[1], RATINGS[4] ),
     new GameModel( "assets/images/mortalkombat.jpg", "Mortal Kombat", "Fighting & Action Game", "mortalkombat", "https://jsemu2.github.io/gba/launcher.html#mortal_kombat", CATEGORIES_LIST[2], RATINGS[3] ),
   ],
+  // GAMES: data,
   CAROUSEL_LIST:[
     new CarouselModel("Pokemon", "An RPG Pokemon Game", "assets/images/pokemon-go-carousel.jpg", "pokemon"),
     new CarouselModel("Mortal Kombat", "Fighting & Action Game", "assets/images/kombat-pack-2.jpg", "mortalkombat")
